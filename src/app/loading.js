122787e7980ce74a323ff89116ace600d3e73e96
Naimur <naimur30447@gmail.com>
2024-05-23 01:36:53 +0600

    clerk

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,9 +1,5 @@
 import Loader from "@/components/Loader/loader";
 
 export default function loading() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-16 h-16 border-t-4 border-b-4 border-[--text] rounded-full animate-spin"></div>
-    </div>
-  );
+  return <Loader />;
 }
