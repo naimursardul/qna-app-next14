d75cb4313adeef8a9f261a7e3458d2ec75634e22
Naimur <naimur30447@gmail.com>
2024-05-23 06:19:18 +0600

    commit

--- src/components/MobileLinks/MobileLinks.jsx
+++ src/components/MobileLinks/MobileLinks.jsx
@@ -4,13 +4,16 @@ import { FaBarsStaggered } from "react-icons/fa6";
 import { RxCross1 } from "react-icons/rx";
 import NavLink from "../Navbar/NavLink/NavLink";
 import { useState } from "react";
+import { UserButton } from "@clerk/nextjs";
 
 export default function MobileLinks({ props }) {
   const { links, session } = props;
 
   const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
+    <div className="flex items-center gap-6">
+      <UserButton />
       <div
         onClick={() => setIsOpen(!isOpen)}
         className="relative text-[--text] z-[60] "
@@ -30,10 +33,10 @@ export default function MobileLinks({ props }) {
           {!session && (
             <div className="flex flex-col gap-3">
               <div onClick={() => setIsOpen(!isOpen)}>
-                <NavLink link={{ name: "Sign up", path: "/signup" }} />
+                <NavLink link={{ name: "Sign up", path: "/sign-up" }} />
               </div>
               <div onClick={() => setIsOpen(!isOpen)}>
-                <NavLink link={{ name: "Log in", path: "/login" }} />
+                <NavLink link={{ name: "Sign in", path: "/sign-in" }} />
               </div>
             </div>
           )}
